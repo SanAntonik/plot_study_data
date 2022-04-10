86c6b18a290feddaa9958734f6fc7ea2b80d7735
SanAntonik <sanantonik@gmail.com>
2022-04-10 22:11:57 +0300

    Tries to plot a histogram

--- study_stats.py
+++ study_stats.py
@@ -16,7 +16,7 @@ def calculate(PATH):
         for line in lines[1::]:
             content = line.split(":")
             if lines[-1] == line:
-            #     sport = int(content[2])
+                #     sport = int(content[2])
                 month_last_day = int(content[0])
             # day = int(content[0])
 
@@ -26,10 +26,11 @@ def calculate(PATH):
             english = int(study[2])
             total = maths + computer_science + english
             study_time_per_day.append(total)
-        
+
         # total_study_time = (maths + computer_science + english) // 60
         mean = sum(study_time_per_day) // month_last_day
-        std = math.sqrt((sum([(day_total - mean)**2 for day_total in study_time_per_day]))/(month_last_day))
+        std = math.sqrt(
+            (sum([(day_total - mean)**2 for day_total in study_time_per_day]))/(month_last_day))
         print(std)
         # print(statistics.stdev(study_time_per_day))
         return study_time_per_day, mean, std
@@ -38,15 +39,24 @@ def calculate(PATH):
 def plot_data(data):
     print(data)
     total_time, mean, std = data
+    print(total_time)
     min_count = 0
+    inside_1std = 0
     for day_total in total_time:
+        if day_total >= (mean-std) and day_total <= (mean+std):
+            inside_1std += 1
         print(day_total)
         min_count += day_total
     print(min_count, min_count//60)
+    print(inside_1std, inside_1std/31)
+
+    fig, ax = plt.subplots()
+
+    ax.hist(total_time)
+
+    plt.show()
+
     plt.plot(total_time)
-    print(mean)
-    # plt.axhline(y=120, color='r', linestyle='-')
-    # plt.axhline(y=180, color='b', linestyle='-')
     plt.axhline(y=mean-std, color='k', linestyle='-')
     plt.axhline(y=mean, color='r', linestyle='-')
     plt.axhline(y=mean+std, color='k', linestyle='-')
